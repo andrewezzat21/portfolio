9b3f4e015698387236a533dda9e714eccdf5704b
Andrew Ezzat <andrewezzat21@gmail.com>
2025-01-05 16:36:07 +0200

    add: homepage

---
+++ src/Components/Line/Line.jsx
@@ -0,0 +1,19 @@
+import styles from './Line.module.css';
+
+// eslint-disable-next-line react/prop-types
+const Rectangle = ({ width, height, top, left, color }) => {
+  return (
+    <div
+      className={styles.rectangle}
+      style={{
+        width: width,
+        height: height,
+        top: top,
+        left: left,
+        backgroundColor: color || 'orange',
+      }}
+    ></div>
+  );
+};
+
+export default Rectangle;
